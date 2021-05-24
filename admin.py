8487b01ba1a69b3c93d6b65a0909870d134d6b71
Emmanuel Owusu <66572915+coding-rev@users.noreply.github.com>
2021-05-24 06:06:01 -0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from .models import Users
 # Register your models here.
-class CustomUsers(admin.ModelAdmin):
-	list_display = ['email','companies_name', 'country']
-	list_filter  = ['country']
-	random_fields= ['email', 'companies_name']
 
-admin.site.register(Users, CustomUsers)
+admin.site.register(Users)
 
 
